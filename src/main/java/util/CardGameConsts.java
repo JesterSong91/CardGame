29832a9472ed05c70d28f8a53167b5b29fa3dc37
Sergei Bashkintcev <jestersong@mail.ru>
2023-11-03 13:00:30 +0400

    Removed redundant constants

--- src/main/java/util/CardGameConsts.java
+++ src/main/java/util/CardGameConsts.java
@@ -1,15 +1,7 @@
 package util;
 
-import java.awt.*;
-
 public class CardGameConsts {
-    public static final int PLAYER_CARDS_LIMIT = 6;
-
     public static final int BOTH_PLAYER_CARDS_LIMIT = 12;
-    public static final Color SELECTED_CARD_COLOR = Color.cyan;
-    public static final Color PLAYED_CARD_COLOR = Color.lightGray;
-
-    public static final Color CURRENT_PLAYER_COLOR = Color.pink;
 
     public static final String PATH_TO_SOUNDS = "C://Users//JesterSong//IdeaProjects//CardGame//sounds//";
     public static final String PATH_TO_IMAGES = "C://Users//JesterSong//IdeaProjects//CardGame//images//";
